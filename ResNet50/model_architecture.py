544c8f54ca55e8ce33220efe333d246ddd1c87da
PV5667 <72002827+PV5667@users.noreply.github.com>
2021-12-22 22:10:22 -0800

    adding first_residual block onto the architecture; corrected model output dimensions

--- ResNet50/model_architecture.py
+++ ResNet50/model_architecture.py
@@ -24,6 +24,32 @@ class ResidualBlock(nn.Module):
     y = F.relu(y)
     return y
 
+
+# the first residual block when going from C! to C2. Helps with the initial dimensionality mismatch while not downsampling the image
+# This falls into the category "B" in the ResNet Paper
+class FirstResidualBlock(nn.Module):
+  def __init__ (self, input_channels, output_channels):
+    super().__init__()
+    self.conv1 = nn.Conv2d(input_channels, output_channels[0], kernel_size = 1, stride = (1,1), bias = False)
+    self.conv2 = nn.Conv2d(output_channels[0], output_channels[0], kernel_size = 3, stride=(1, 1), padding = (1,1), bias = False)
+    self.conv3 = nn.Conv2d(output_channels[0], output_channels[1], kernel_size = 1, stride = (1,1), bias = False)
+    self.bn1 = nn.BatchNorm2d(output_channels[0])
+    self.bn2 = nn.BatchNorm2d(output_channels[0])
+    self.bn3 = nn.BatchNorm2d(output_channels[1])
+    self.conv1x1 = nn.Conv2d(input_channels, output_channels[1], kernel_size = (1,1), stride = (1,1), bias = False)
+  def forward(self, x):
+    #print(x.shape)
+    y = F.relu(self.bn1(self.conv1(x)))
+    #print(y.shape)
+    y = F.relu(self.bn2(self.conv2(y)))
+    #print(y.shape)
+    y = self.conv3(y)
+    #print(y.shape)
+    y = self.bn3(y)
+    y = y + self.conv1x1(x)
+    y = F.relu(y)
+    return y
+
 class ResidualBlockwith1x1(nn.Module):
   def __init__ (self, input_channels, output_channels):
     super().__init__()
@@ -47,7 +73,7 @@ class Resnet50(nn.Module):
     super().__init__()
     self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride = (2,2), padding = 3)
     self.pool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = (1,1))
-    self.conv2 = nn.Sequential(ResidualBlockwith1x1(64, [64, 256]), ResidualBlock(256, [64, 256]))
+    self.conv2 = nn.Sequential(FirstResidualBlock(64, [64, 256]), ResidualBlock(256, [64, 256]), ResidualBlock(256, [64, 256]))
     self.conv3 = nn.Sequential(ResidualBlockwith1x1(256, [128, 512]), ResidualBlock(512, [128, 512]), ResidualBlock(512, [128, 512]), ResidualBlock(512, [128, 512]))
     self.conv4 = nn.Sequential(ResidualBlockwith1x1(512, [256, 1024]), ResidualBlock(1024, [256, 1024]), ResidualBlock(1024, [256, 1024]), ResidualBlock(1024, [256, 1024]), ResidualBlock(1024, [256, 1024]), ResidualBlock(1024, [256, 1024]))
     self.conv5 = nn.Sequential(ResidualBlockwith1x1(1024, [512, 2048]), ResidualBlock(2048, [512, 2048]), ResidualBlock(2048, [512, 2048]))
@@ -55,17 +81,17 @@ class Resnet50(nn.Module):
     self.classifier = nn.Sequential( nn.Flatten(), nn.Linear(2048, 10))
   def forward(self, x):
     x = F.relu(self.conv1(x))
-    #print(x.shape)
+    print(x.shape)
     x = self.pool(x)
-    #print(x.shape)
+    print(x.shape)
     x = self.conv2(x)
-    #print(x.shape)
+    print(x.shape)
     x = self.conv3(x)
-    #print(x.shape)
+    print(x.shape)
     x = self.conv4(x)
-    #print(x.shape)
+    print(x.shape)
     x = self.conv5(x)
-    #print(x.shape)
+    print(x.shape)
     x = self.avgpool(x)
     #print(x.shape)
     x = self.classifier(x)
