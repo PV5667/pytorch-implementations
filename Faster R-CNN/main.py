d7b1149aae9a83107903e9e5de8cd628f9ac73b4
PV5667 <72002827+PV5667@users.noreply.github.com>
2021-12-23 19:51:14 -0800

    describe implementation of Faster R-CNN

--- Faster R-CNN/main.py
+++ Faster R-CNN/main.py
@@ -1,1 +1,17 @@
 # The main file where the main network is defined and trained, using imported classes
+
+
+
+"""
+for each feature map outputted by the FPN, a convolution of 3 x 3 with a padding of 1 is applied
+this is to make sure that jthe result has "c" channels, which is then fed into the anchor prediction (c is the RGB, etc, etc)
+
+from the result of this convolution, anchor boxes are generated centered on each pixel
+One difference to notice here because we are using FPN is that there is no need for multiscale anchor boxes, as this is taken by FPN being inherently multiscale
+Furthermore, in this implementation, we will not be adding the P6 layer as a feature map to input to the RPN
+
+The anchor boxes are then fed into two conv2D layers; one is for predicting thej objectness score of the anchor box (this is binary) and the other is for predicting the offsets to the anchors
+These predictions are then fed into non-max suppression, after which they are fed into RoI pooling(check Fast RCNN) 
+The formula for the ROI layer's height and width for layer P(k) is: k = k(0) + log2(sqrt(wh)/224). 224 is the scale of the training image (if using ImageNet). k(0) = 4
+After this, they are sent through fully connected layers which will output class predictions 
+"""
