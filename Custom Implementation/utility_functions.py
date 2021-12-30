d4f7ac6590b963932292d958b66723aacda14804
PV5667 <72002827+PV5667@users.noreply.github.com>
2021-12-30 15:31:20 -0800

    commit box matching function

--- Custom Implementation/utility_functions.py
+++ Custom Implementation/utility_functions.py
@@ -253,3 +253,36 @@ test = grid_anchors(feature_map_sizes, strides, base_anchors)
 for i in test:
   print("-------------||||||||||---------")
   print(bbox_to_center(i).shape)
+
+def match_boxes(ground_truth, anchors, iou_thresh):
+  num_bboxes, num_anchors = len(ground_truth), len(anchors)
+
+  jaccard_matrix = torchvision.ops.box_iou(anchors, ground_truth)
+
+  pair_map = torch.ones((num_anchors,), dtype = torch.long  ) * -1
+  max_ious, indices = torch.max(jaccard_matrix, dim = 1)
+  #print(indices)
+  anchor = torch.nonzero(max_ious >= iou_thresh).reshape(-1)
+  box = indices[max_ious >= iou_thresh]
+  #print(anchor)
+  #print(box)
+  #print(pair_map)
+  pair_map[anchor] = box
+  #print(pair_map)
+
+
+  fill_col = torch.ones((num_anchors, ), dtype = torch.long) * -1
+  fill_row = torch.ones((num_bboxes, ), dtype = torch.long) * -1
+
+
+  for i in range(num_bboxes):
+    max_idx = torch.argmax(jaccard_matrix)
+    #print(max_idx)
+    b_j = (max_idx % num_bboxes).long()
+    a_i = (max_idx / num_bboxes).long()
+
+    pair_map[a_i] = b_j
+
+    jaccard_matrix[:, b_j] = fill_col
+    jaccard_matrix[a_i, :] = fill_row
+  return pair_map
