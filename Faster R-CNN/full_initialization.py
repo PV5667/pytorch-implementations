821b51e71a18716e57786792b1766b86c5a6f51c
PV5667 <72002827+PV5667@users.noreply.github.com>
2022-01-17 18:20:23 -0800

    initialize faster rcnn class

--- Faster R-CNN/full_initialization.py
+++ Faster R-CNN/full_initialization.py
@@ -148,3 +148,79 @@ def grid_anchors(map_sizes, strides, base_anchors):
 
     output.append((result.view(-1, 1 , 4) + anchors_to_be_projected).reshape(-1, 4))
   return output 
+
+
+
+class Faster_RCNN(nn.Module):
+  def __init__(self, in_channels, num_anchors, num_classes):
+    super().__init__()
+    self.num_anchors = num_anchors
+    self.num_classes = num_classes
+    self.VGG16 = VGG16_BackBone(in_channels)
+    self.prep_conv_RPN = nn.Conv2d(512, 512, kernel_size = (3,3), padding = 1)
+
+    self.fc = nn.Sequential(nn.Conv2d(512, 4096, kernel_size=(1,1), stride = (1,1), bias= False), nn.ReLU(), nn.Conv2d(4096, 4096, kernel_size=(1,1), stride = (1,1), bias= False), nn.ReLU())
+
+    self.final_class_layer = nn.Conv2d(4096, self.num_classes, kernel_size = (1, 1), stride = (1,1), bias = False)
+    self.final_bbox_layer = nn.Conv2d(4096, 4, kernel_size = (1,1), stride = (1,1), bias = False)
+    # define the prediction heads, use result of nms, take its .size()[0]
+
+  def forward(self, x):
+    feature_map = self.VGG16(x)
+    # Run this feature map through the RPN and a conv layer
+    print("Feature map shape: ", feature_map.shape)
+
+    # RPN
+    RPN_map = F.relu(self.prep_conv_RPN(feature_map))
+    print("RPN input map shape: ", RPN_map.size())
+    # generate template anchors
+    template_anchors = grid_anchors(feature_map_sizes, strides, base_anchors)
+    #anchors = {GENERATE TEMPLATE ANCHORS HERE FOR THE FEATURE MAP}
+    binary_preds = binary_pred(self.num_anchors)(RPN_map)
+    
+
+    binary_preds = binary_preds.reshape(binary_preds.shape[0], -1, 1)
+    binary_preds = binary_preds.view(-1)
+
+    print(binary_preds.shape)
+
+    offset_preds = box_pred(self.num_anchors)(RPN_map)
+    offset_preds = offset_preds.reshape(offset_preds.shape[0], -1, 4)
+
+    #print(len(template_anchors)) 
+    #print(offset_preds.shape)
+
+    template_anchors = torch.cat(template_anchors, dim = 0)
+    template_anchors = template_anchors.view(1, -1, 4)
+
+    print(template_anchors.shape)
+    # use nms built-in function to filter predictions
+    boxes_converted = offsets_to_bboxes(template_anchors, offset_preds)
+    boxes_converted = boxes_converted.view(-1, 4)
+
+    print("Boxes", boxes_converted.shape)
+    
+    filtered_preds = torchvision.ops.nms(boxes_converted, binary_preds, 0.7)
+
+    filtered_preds = filtered_preds.view(-1, 5).float()
+    print(filtered_preds.shape)
+    # use roi pooling built-in function  
+
+
+    result = torchvision.ops.roi_pool(feature_map, filtered_preds, (7, 7))
+
+    print(result.shape)
+
+    result = self.fc(result)
+    print(result.shape)
+
+    class_preds = self.final_class_layer(result)
+    bbox_preds = self.final_bbox_layer(result)
+
+    return class_preds, bbox_preds 
+
+
+
+net = Faster_RCNN(3, 9, 10)
+t = torch.randn(1,3, 224, 224)
+net.forward(t)
