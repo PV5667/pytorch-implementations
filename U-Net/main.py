5bc148c61a2e1e9897d8c999ee6f29aa5061855f
PV5667 <72002827+PV5667@users.noreply.github.com>
2022-01-22 09:07:04 -0800

    commit

--- U-Net/main.py
+++ U-Net/main.py
@@ -4,12 +4,19 @@ import torch.nn as nn
                                                                                                                                             
                                                                                                                                             
                                                                                                                                             
-class UNET_Block():                                                                                                                         
-  def __init__(self, input_channels, output_channels):                                                                                      
-    """Initializes one of the blocks of the U-Net architecture."""                                                                          
-    super().__init__()                                                                                                                      
-    self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=(3,3), stride = (1,1), padding = (1, 1), bias = False)                                                                        
-    self.conv2 = nn.Conv2d(input_channels, output_channels, kernel_size=(3,3), stride = (1,1), padding = (1, 1), bias = False)                                                                        
+
+                                                                                                                                            
+def UNet_Block(channels):
+  """Define the UNet repeating blocks"""
+  input_channels = channels[0]
+  output_channels = channels[1]
+  layers = nn.Sequential(nn.Conv2d(input_channels, output_channels, kernel_size = (3,3), stride = (1,1), padding = (1, 1), bias = False), nn.BatchNorm2d(output_channels), nn.ReLU(inplace = True),
+                         nn.Conv2d(output_channels, output_channels, kernel_size = (3,3), stride = (1,1), padding = (1,1), bias = False), nn.BatchNorm2d(output_channels), nn.ReLU(inplace = True))
+  return layers                                                       
+                                                                                                                                            
+def conv1x1(input_channels):
+  return nn.Sequential(nn.Conv2d(input_channels, input_channels, kernel_size = (1,1), stride = (1, 1), bias = False))
+                                                           
                                                                                                                                             
 class UNet():                                                                                                                               
   def __init__(self, input_channels, output_channels = 2):                                                                                  
