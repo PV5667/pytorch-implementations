f1c43a5064d91064770d59057172710c729fe2e3
PV5667 <72002827+PV5667@users.noreply.github.com>
2022-01-22 11:38:11 -0800

    commit

--- U-Net/main.py
+++ U-Net/main.py
@@ -18,33 +18,56 @@ def conv1x1(input_channels):
   return nn.Sequential(nn.Conv2d(input_channels, input_channels, kernel_size = (1,1), stride = (1, 1), bias = False))
                                                            
                                                                                                                                             
-class UNet():                                                                                                                               
-  def __init__(self, input_channels, output_channels = 2):                                                                                  
-    super().__init__()                                                                                                                      
-    self.maxpool = nn.MaxPool2d(kernel_size = (2, 2), stride = 2)
-    self.upsample = nn.Upsample(scale_factor = 2, mode = 'nearest')
-    self.channels = [
-            [input_channels, 64]
+class UNet(nn.Module):                                                                                                                               
+  def __init__(self, input_channels, output_channels = 2, num_classes = 2):                                                                                  
+    super().__init__()
+                                                                                                                          
+    self.down_channels = [
+            [input_channels, 64],
             [64, 128],
             [128, 256],
             [256, 512],           
-            [512, 1024],
+            [512, 1024]
+            ]
+    self.up_channels = [
             [1024, 512],
-            [512, 256],
-            [256, 128],
+            [512, 256], 
+            [256, 128], 
             [128, 64]
             ]
+    self.sizes = [56, 104, 200, 392]
+    self.num_classes = num_classes 
 
   def forward(self, x):
-    level_1_left = UNet_Block(self.channels[0])(x)
-    level_2_left_in = self.maxpool(level_1_left)
-    level_2_left = UNet_Block(self.channels[1])(level_2_left_in)
-    level_3_left_in = self.maxpool(level_2_left)
-    level_3_left = UNet_Block(self.channels[2])(level_3_left_in)
-    level_4_left_in = self.maxpool(level_3_left)
-    level_4_left = UNet_Block(self.channels[3])(level_4_left_in)
-    level_5_in = self.maxpool(level_4_left)
-    level_5 = UNet_Block(self.channels[4])(level_5_in)
+    print(x.shape)
+    level_1_left = first_block(self.down_channels[0], x)
+    print(level_1_left.shape)
 
-    level_4_right_in = self.upsample(level_5)
-    level_4_right = UNet_Block() 
+    level_2_left = UNet_Down_Block(self.down_channels[1], level_1_left)
+    print(level_2_left.shape)
+
+    level_3_left = UNet_Down_Block(self.down_channels[2], level_2_left)
+    print(level_3_left.shape)
+
+    level_4_left = UNet_Down_Block(self.down_channels[3], level_3_left)
+    print(level_4_left.shape)
+
+    level_5 = UNet_Down_Block(self.down_channels[4], level_4_left)
+    print(level_5.shape)
+
+    level_4_right = UNet_Up_Block(self.up_channels[0], self.sizes[0], level_4_left, level_5)
+    print(level_4_right.shape)
+
+    level_3_right = UNet_Up_Block(self.up_channels[1], self.sizes[1], level_3_left, level_4_right)
+    print(level_3_right.shape)
+
+    level_2_right = UNet_Up_Block(self.up_channels[2], self.sizes[2], level_2_left, level_3_right)
+    print(level_2_right.shape)
+
+    level_1_right = UNet_Up_Block(self.up_channels[3], self.sizes[3], level_1_left, level_2_right)
+    print(level_1_right.shape)
+
+    result = conv1x1(level_1_right.shape[1], self.num_classes)(level_1_right)
+    print(result.shape)
+
+    return result
